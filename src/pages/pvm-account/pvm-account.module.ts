c1f9863ccc37932ee24cd832f03a26ca47f240c9
adivich <adivich@gmail.com>
2018-02-26 23:10:48 +0200

    multiline ellipsis not really working

--- src/pages/pvm-account/pvm-account.module.ts
+++ src/pages/pvm-account/pvm-account.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PvmAccountPage } from './pvm-account';
+//import {PvmDetailsComponent} from "../../components/pvm-details/pvm-details";
+import {ComponentsModule} from "../../components/components.module";
 
 @NgModule({
   declarations: [
@@ -8,6 +10,7 @@ import { PvmAccountPage } from './pvm-account';
   ],
   imports: [
     IonicPageModule.forChild(PvmAccountPage),
+    ComponentsModule
   ],
   exports:[PvmAccountPage]
 })
